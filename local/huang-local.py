d2435cd7da5fd425cfdca8cdc38572ed219c6f90
0u812 <0u812@users.noreply.github.com>
2017-06-28 22:57:15 -0700

    notes

--- local/huang-local.py
+++ local/huang-local.py
@@ -4,3 +4,7 @@ import tellurium as te
 model = te.loadAntimonyModel(te.sbmlToAntimony('huang-ferrell-96.xml'))
 model.conservedMoietyAnalysis = True
 print(model.steadyStateNamedArray())
+
+# the output variable is PP_K (which stands for double phosphorylated MAPK)
+
+# parameter 2: r1b_k2
